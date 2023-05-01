3a2ad79d5a98c15c78fcd9c91e1f4b9e5fa38a62
Matthew D. Scholefield <matthew331199@gmail.com>
2023-04-30 22:08:19 -0700

    Increment version to 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='unitranscode',
-    version='0.3.0',
+    version='0.3.1',
     description='Universal transcoding library',
     url='https://github.com/MatthewScholefield/unitranscode',
     author='Matthew D. Scholefield',
